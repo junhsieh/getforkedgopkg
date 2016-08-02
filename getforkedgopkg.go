14527cecdff367aee2940f147957439bcae150d9
Jun Hsieh <jun.github@ijun.org>
2016-08-02 10:10:50 -0700

    Update

--- getforkedgopkg.go
+++ getforkedgopkg.go
@@ -21,53 +21,64 @@ const (
 )
 
 var (
-	githubUsernamePtr *string = flag.String("githubUsername", "junhsieh", "GitHub Username")
-	goPath            string  = os.Getenv("GOPATH")
-	githubSrc         string  = goPath + "/" + githubSrcRelative
+	githubUsernamePtr = flag.String("githubUsername", "junhsieh", "GitHub Username")
+	goPath            = os.Getenv("GOPATH")
+	githubSrc         = goPath + "/" + githubSrcRelative
 )
 
+// Repo ...
 type Repo struct {
 	UpstreamUsername string
 	Name             string
 	GoGetURL         string
 }
 
+// GetOriginSSH ...
 func (r *Repo) GetOriginSSH() string {
 	return githubSSH + ":" + *githubUsernamePtr + "/" + r.Name + ".git"
 }
 
+// GetUpstreamHTTPS ...
 func (r *Repo) GetUpstreamHTTPS() string {
 	return githubHTTPS + "/" + r.UpstreamUsername + "/" + r.Name + ".git"
 }
 
+// GetSrcDir ...
 func (r *Repo) GetSrcDir() string {
 	return githubSrc
 }
 
+// GetOriginDir ...
 func (r *Repo) GetOriginDir() string {
 	return r.GetSrcDir() + "/" + *githubUsernamePtr
 }
 
+// GetUpstreamDir ...
 func (r *Repo) GetUpstreamDir() string {
 	return r.GetSrcDir() + "/" + r.UpstreamUsername
 }
 
+// GetCMDBash ...
 func (r *Repo) GetCMDBash(cmd string) string {
 	return bashBin + " -c '" + cmd + "'"
 }
 
+// GetCMDClone ...
 func (r *Repo) GetCMDClone() string {
 	return r.GetCMDBash(cdBin + " " + r.GetOriginDir() + "/" + r.Name + " && " + gitBin + " clone " + r.GetOriginSSH())
 }
 
+// GetCMDRemoteAdd ...
 func (r *Repo) GetCMDRemoteAdd() string {
 	return r.GetCMDBash(cdBin + " " + r.GetOriginDir() + "/" + r.Name + " && " + gitBin + " remote add upstream " + r.GetUpstreamHTTPS())
 }
 
+// GetCMDLink ...
 func (r *Repo) GetCMDLink() string {
 	return lnBin + " -s " + r.GetOriginDir() + "/" + r.Name + " " + r.GetUpstreamDir() + "/" + r.Name
 }
 
+// Run ...
 func (r *Repo) Run() error {
 	if r.UpstreamUsername == *githubUsernamePtr {
 		return errors.New("GitHub username and upstream username could not be the same")
